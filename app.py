355846aaca386b24032345cfe026e56cab90ff5c
Daeryeok Ko <rheofur@kaist.ac.kr>
2025-09-17 11:30:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("🔬 Main Page")
+st.title("🔬 Welcome to the In-Lab Calculator!")
 st.sidebar.success("Select a calculator above.")
 
 st.markdown(
