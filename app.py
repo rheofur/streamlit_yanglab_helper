4c88d28ad247f24461709c789ab02a1f305ec2a0
Daeryeok Ko <rheofur@kaist.ac.kr>
2025-09-17 11:42:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,14 @@ st.set_page_config(
     layout="wide"
 )
 
-# This command automatically navigates to your desired main page
+# This CSS hides the first list item in the sidebar navigation, which is the "app" page.
+st.markdown("""
+    <style>
+        [data-testid="stSidebarNav"] ul > li:first-child {
+            display: none;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+
+# This command automatically navigates to your desired main page on first load.
 st.switch_page("pages/1_Main.py")
